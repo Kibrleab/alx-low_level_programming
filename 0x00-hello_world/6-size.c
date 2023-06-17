7d54e7a7b6140bcffa2d3927ffe4928a010dd666
kibrleab <kbelija84@gmail.com>
2023-06-17 05:59:13 -0700

    w

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,5 +6,15 @@
  */
 int main(void)
 {
-
+int u;
+long int w;
+long long int x;
+char y;
+float z;
+printf("size of an int: %d byte(s)\n", sizeof(u));
+printf("size of a long int: %d byte(s)\n", sizeof(w));
+printf("size of a long long int: %d byte(s)\n", sizeof(x));
+printf("size of a char: %d byte(s)\n", sizeof(y));
+printf("size of a float: %d byte(s)\n", sizeof(z);
+return (0);
 }
