edec7ff79434776b246437be4c9ffc3513243a12
kibrleab <kbelija84@gmail.com>
2023-06-17 11:46:39 -0700

    w

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - The alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+continue 'e', 'q';
+l++;
+}
+
+putchar('\n');
+
+return (0);
+}
