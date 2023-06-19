817235e36a8b564ccca02a710ceead85e6f94b33
kibrleab <kbelija84@gmail.com>
2023-06-19 05:35:04 -0700

    w

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - All possible different combinations of three digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int n, m, o;
+for (n = '0'; n <= '9'; n++)
+{
+for (m = '0'; m <= '8'; m++)
+{
+for (o = '0'; o <= '7'; o++)
+{
+if (o != m && o != n && m != n)
+{
+putchar(n);
+utchar(m);
+putchar(o);
+if (!(n == '9' && m == '8' && o == '7'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
