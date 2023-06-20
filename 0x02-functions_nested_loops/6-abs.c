9a94140d8882214844f7eee2127f4f90cb55e92c
kibrleab <kbelija84@gmail.com>
2023-06-20 13:09:03 -0700

    w

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _abs - Alphabet in lowercase
+ * @x: number
+ * Return: Absolut value
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	
+		x = (-1) * x;
+	return (x);
+	
+	_putchar('\n');
+}
