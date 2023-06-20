c1f6f92e8270fb9dec309b34260a4bf8af1eff92
kibrleab <kbelija84@gmail.com>
2023-06-20 13:26:52 -0700

    w

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_last_digit Print last digit
+ * @z: number
+ * Return: Laste digit
+ */
+int print_last_digit(int z)
+{
+	int ld;
+if (z < 0)
+       ld = -1 * (z % 10);
+else
+	ld = z % 10;
+return (ld);
+_putchar('\n');
+}
