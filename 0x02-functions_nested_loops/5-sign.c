67b3782c5d83966ae22ffd4248a161befd5c583a
kibrleab <kbelija84@gmail.com>
2023-06-20 12:15:47 -0700

    w

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,22 +11,22 @@ int print_sign(int n)
 		_putchar('+');
 		_putchar(',');
 		_putchar(' ');
-		return (1);
+		 return (1);
 		_putchar('\n');
 	}
 	else if (n == 0)
 	{
-		_putchar(0);
+		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
 		return (0);
 		_putchar('\n');
 		}
-	else if (n < 0)
-		{
+	else
+	{
 		_putchar('-');
 		_putchar(',');
 		_putchar(' ');
 		return (-1);
 		}
-	}
+}	
