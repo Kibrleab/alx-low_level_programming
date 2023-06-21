68f2b39715fbb6b1106796be0273c4d119422212
kibrleab <kbelija84@gmail.com>
2023-06-20 17:10:46 -0700

    e

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_to_98 - prints all natural numbers from n to 98
+ *@n: initial number
+ *Return: ...
+ */
+void print_to_98(int n)
+{
+	for (n = 20; n <= 98; n++)
+	{
+		_putchar(n);
+		-putchar(' ');
+	}
+	_putchar('\n');
+}
