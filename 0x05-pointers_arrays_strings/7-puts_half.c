a3d77f2a0ecde99bedb3fc7bef2baf5127f64118
kibrleab <kbelija84@gmail.com>
2023-06-26 13:46:11 -0700

    d

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ *@str: pointer to string
+ *
+ */
+void puts_half(char *str)
+{
+	int odd, j, i;
+
+	while (str[odd] != '\0')
+	{
+		odd++;
+	}
+	if (odd % 2 == 0)
+		for (i = odd / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	else if (odd % 2)
+	{
+		for (j = (odd -1) / 2; j < odd - 1; j++)
+			_putchar(str[j + 1]);
+	}
+}
+_putchar('\n');
+}
