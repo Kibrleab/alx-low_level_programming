2d9cea33275a7f8d49dbe4ff5fe79ee09c5b32b1
kibrleab <kbelija84@gmail.com>
2023-06-26 15:16:42 -0700

    f

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
- *
- *
+ * print_rev - print a sting in revars
+ *@s: pointer to string
  *
  */
 void print_rev(char *s)
 {
 	int x, y, k;
+	
+	x = 0;
+
 	while (s[x] != '\0')
 	{
 		x++;
 	}
-k = x;
-	for (y = k-1; y >= 0; y--)
+	k = x;
+
+	for (y = k - 1; y >= 0; y--)
 	{
 		_putchar(s[y]);
 	}
