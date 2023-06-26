642f887c53b1f679513a4eeea2e7a50228262baf
kibrleab <kbelija84@gmail.com>
2023-06-26 10:08:28 -0700

    e

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,15 @@
 #include "main.h"
 /**
- *
- *
+ *_puts - unction that prints a string
+ *@str: pointer
+ *Return: nothing
  */
 void _puts(char *str)
 {
-
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+	_putchr(i);
+}
+_putchar('\n';
+		}
