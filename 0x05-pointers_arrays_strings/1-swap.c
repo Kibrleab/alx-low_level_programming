b272e9c94a6ebd6a4eddecd1af1b56feed8dc060
kibrleab <kbelija84@gmail.com>
2023-06-26 07:55:11 -0700

    w

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * swap_int - unction that swaps the values of two integers.
+ * @a: the firest num
+ * @b: the second num
+ * @z: integer used to swap
+ */
+void swap_int(int *a, int *b)
+{
+	a = 98;
+	b = 42;
+	_putchar(a '\t');
+	_putchar(b '\n');
+	int z = a;
+	a = b;
+	b = z;
+	_putchar(a '\t');
+        _putchar(b '\n');
+}
