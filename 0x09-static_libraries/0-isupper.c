57e8dd8bf5284916edc477797fa137f50bcd1366
kibrleab <kbelija84@gmail.com>
2023-07-10 01:30:25 -0700

    d

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -8,11 +8,8 @@
 
 #include <string.h>
 
-
-int _putchar(char c)
+int _isupper(int c)
 
 {
-
-    return putchar(c);
-
+ return (c >= 'A' && c <= 'Z');
 }
--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -9,10 +9,10 @@
 #include <string.h>
 
 
-int _putchar(char c)
+int _islower(int c)
 
 {
 
-    return putchar(c);
+    return (c >= 'a' && c <= 'z');
 
 }
