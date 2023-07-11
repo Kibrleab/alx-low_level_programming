5541866f814a6ba25e2c0c29b29c5c4aa4ac33dd
kibrleab <kbelija84@gmail.com>
2023-07-11 12:23:07 -0700

    d

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	pstr = malloc(sizeof(char) * size);
-if (size == 0 || str == NULL)
+if (size == 0 ||pstr == NULL)
 	return (NULL);
 i = 0;
 while (i < size)
